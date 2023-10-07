8b283aeacaddf902d31da05bbc5fc17f37e11b37
quantulr <35954003+quantulr@users.noreply.github.com>
2023-10-07 23:35:43 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+import { Command } from "@tauri-apps/api/shell";
+
+const winswVer = async () => {
+  const command = Command.sidecar("bin/winsw", "--version");
+
+  command.on("close", (data) => {
+    console.log(
+      `command finished with code ${data.code} and signal ${data.signal}`,
+    );
+  });
+  command.on("error", (error) => console.error(`command error: "${error}"`));
+
+  command.stdout.on("data", (line) => {
+    console.log(line);
+  });
+  command.stderr.on("data", (line) => {
+    console.log(line);
+  });
+  await command.spawn();
+
+};
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <div className="container">
+      <button
+        onClick={() => {
+          winswVer();
+        }}
+      >
+        winsw version
+      </button>
+      <h1>Welcome to Tauri!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
